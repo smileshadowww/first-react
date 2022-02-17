ea138982a0657ab439d0b1d10cafbb59dd48f41e
Rafał Stekla <steklarafal13@gmail.com>
2022-02-17 18:04:03 +0100

    m11

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,7 +3,7 @@ import styles from './Container.module.scss'
 const Container = props => {
     return (
         <div className={styles.container}>
-          {props.children};
+          {props.children}
         </div>
     );
 };
