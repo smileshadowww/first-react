fa4aaaa9936dc7489f73c2a3c2bcf0e56815ed08
Rafał Stekla <steklarafal13@gmail.com>
2022-02-20 15:26:51 +0100

    ADD_CARD.m13.3

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -2,17 +2,18 @@ import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
 import Button from './../Button/Button';
 import TextInput from './../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
 
 const ColumnForm = props => {
     const [title, setTitle] = useState('');
     const [icon, setIcon] = useState('');
     const handleSubmit = e => {
-        e.preventDefault();
-        props.action({ title: title, icon: icon });
-        setTitle('');
-        setIcon('');
+      e.preventDefault();
+      dispatch({ type: 'ADD_COLUMN', payload: { title, icon } });
+      setTitle('');
+      setIcon('');
     };
-
+    const dispatch = useDispatch();
 	return (
     <form onSubmit={handleSubmit}>
         Title: <TextInput value={title} onChange={e => setTitle(e.target.value)} />
