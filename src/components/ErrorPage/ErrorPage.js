4c5c7141d3a4e6267946ce25b39f3003f5284e13
Rafał Stekla <steklarafal13@gmail.com>
2022-02-20 20:06:18 +0100

    404NOTFOUNDm14.1

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -5,7 +5,7 @@ const ErrorPage = () => {
     return (
       <div className={styles.hero}>
         <PageTitle>404 not found</PageTitle>
-        <p className={styles.subtitle}>Please use correct url, click on one of navigation bars or <span href="/">CLICK HERE</span></p>
+        <p className={styles.subtitle}>Please use correct url, click on one of navigation bars or <a href="/"><span>CLICK HERE</span></a></p>
       </div>
     );
   };
